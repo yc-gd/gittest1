2f24275f775905252279081bf408a7b287e92ad4
chu <mulingtest@163.com>
2020-08-21 12:13:53 +0800

    2

--- src/main/java/com/yc/App.java
+++ src/main/java/com/yc/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello111 World!哈哈" );
+        System.out.println( "Hello111 World!哈哈456" );
     }
 }
